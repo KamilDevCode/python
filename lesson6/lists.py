34021b58ccba49fc49cc90cc9034b9e12ded4790
Kamil Lewandowski <kamlew555@gmail.com>
2024-03-25 20:01:33 +0100

    working with lists: remove, pop, del sort methods

--- lesson6/lists.py
+++ lesson6/lists.py
@@ -8,25 +8,25 @@ data = [True, 44, "Kamil"]
 
 
 
-# lists are zero-indexed
+# # lists are zero-indexed
 print(users[0]) # prints 'Kamil'
 
-# check lasts element of list
+# # check lasts element of list
 print(users[-1]) # prints 'Jan'
 
 
-# checking position of element
+# # checking position of element
 print(users.index('Jan')) # prints 2
 
-# checking range of elements of list 
+# # checking range of elements of list 
 
 print(users[0:2]) # prints ['Kamil', 'Adam']
 
-# checking range of elements of list 
+# # checking range of elements of list 
 
 print(users[0:]) # prints ['Kamil', 'Adam', 'Jan']
 
-#  lists are mutable
+# #  lists are mutable
 users[0] = 'Jan'
 
 print(users) # prints ['Jan', 'Adam', 'Jan']
@@ -34,20 +34,20 @@ print(users) # prints ['Jan', 'Adam', 'Jan']
 print(users[1]) # prints 'Adam' 
 
 
-# check if 'Adam' is in the list
+# # check if 'Adam' is in the list
 print("Adam" in users) # True because 'Adam' is in the list
 
 
 print('Jan' not in users) # False beacause 'Jan' is in the list
 
 
-# checking length of list with len() function
+# # checking length of list with len() function
 print(len(users)) # prints 3
 
-# adding username to list of users at the end of the list
+# # adding username to list of users at the end of the list
 users.append('Eliza')
 
-# adding usersname to list of users at the beginning of the list
+# # adding usersname to list of users at the beginning of the list
 users += ['Darek']
 
 
@@ -58,5 +58,28 @@ users.insert(0, 'Piotr')
 
 users[1:3] = ["Bożena", "Jacek"]
 
+# removing element from list
+users.remove('Piotr')
+ 
+print(users.pop(0))
+
+# deleting element from list with specified index
+
+del users[0]
+
+# clearing list of users, but list itself exists
+
+users.clear()
+
+users[1:2] = ['kamil']
+ 
+ 
+# making list of users in alphabetical order
+users.sort()
+
+users.sort(key = str.lower)
+ 
+ 
 print(users)
+ 
 
