d59067bb37230f461c2dacae7416993cc6559ba7
Kamil Lewandowski <kamlew555@gmail.com>
2024-04-07 16:52:58 +0200

    working with modules in python

---
+++ lesson14/my_math.py
@@ -0,0 +1,8 @@
+import math
+print(math.pi)
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
---
+++ lesson14/result.py
@@ -0,0 +1,12 @@
+import my_math # import module
+from math import pi # import specific elements from the module using the from ... import statement.
+
+print(pi)
+
+result = my_math.add(5,5)
+
+print(result)
+
+result = my_math.subtract(5,2)
+
+print(result)
