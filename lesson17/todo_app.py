5915823965a5d04e456bf5e55536b53a581096fc
Kamil Lewandowski <kamlew555@gmail.com>
2024-05-17 14:03:36 +0200

    create todo list and add task function

--- lesson17/todo_app.py
+++ lesson17/todo_app.py
@@ -1,2 +1,12 @@
+tasks = []
 
+
+def add_tasks(new_task=str, definition=str, priority=str):
+    if not new_task.strip():
+        print('You did not add task to the lists !')
+        return
+    else:
+        tasks.append({"name": new_task, "definition": definition, "priority": priority})
+        print(f'You added task: {new_task} with definition: {definition} and priority: {priority} to the list.')
+        print(tasks)
 
